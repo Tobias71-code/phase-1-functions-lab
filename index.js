fa3f27a88bc7e1d0b5bcf1ead6f0789da9a919d7
TobiasNethan <sbttobias@gmail.com>
2024-10-03 09:49:14 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hq = 42;
+    return Math.abs(someValue - hq);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return Number(((distanceInFeet - 400) * 0.02).toFixed(2));  // Convert back to a number
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
